de316dedc9107b7afe9ba72ba017151a8a465ca6
Yara-Silva05 <yara.silva.alves05@gmail.com>
2025-11-08 02:44:56 -0300

    feat: integrando API

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo>{
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
@@ -68,4 +68,10 @@ public class Titulo {
     public String toString() {
         return this.getNome();
     }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
 }
--- src/br/com/alura/screenmatch/principal/MainComListas.java
+++ src/br/com/alura/screenmatch/principal/MainComListas.java
@@ -5,13 +5,26 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainComListas {
     public static void main(String[] args) {
 
         Filme primeiroFilme = new Filme("O pianista", 2002);
+        primeiroFilme.avalia(6);
+        primeiroFilme.avalia(8.5);
+        primeiroFilme.avalia(9);
+
         Filme segundoFilme = new Filme("ELA", 2013);
+        segundoFilme.avalia(9.8);
+        segundoFilme.avalia(9);
+        segundoFilme.avalia(7);
+
         Filme terceiroFilme = new Filme("Ta chovendo hamburguer", 2009);
+        terceiroFilme.avalia(10);
+        terceiroFilme.avalia(7.5);
+        terceiroFilme.avalia(8);
+
         Serie theWalkingDead = new Serie("The walking dead",2009 );
 
         ArrayList<Titulo> listaDeTitulos = new ArrayList<>();
@@ -20,9 +33,18 @@ public class MainComListas {
         listaDeTitulos.add(terceiroFilme);
         listaDeTitulos.add(theWalkingDead);
         System.out.println("Tamanho da lista: " + listaDeTitulos.size());
+        System.out.println("");
 
+        Collections.sort(listaDeTitulos);
+
+        System.out.println(listaDeTitulos);
+        
         for (Titulo item: listaDeTitulos) {
             System.out.println(item);
+
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
         }
     }
 }
